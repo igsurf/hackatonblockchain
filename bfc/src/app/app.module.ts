b907327dd29af9f0b25f1fa544e4e850c041e5c6
Roque <thiago.a.roque@accenture.com>
2017-07-22 17:51:17 -0300

    ajuste da rota do ApplicationRoutesComponent

--- bfc/src/app/app.module.ts
+++ bfc/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ApplicationRoutesComponent } from './routes/src/app/routes/application-routes/application-routes.component';
+import { ApplicationRoutesComponent } from './routes/application-routes/application-routes.component';
 
 @NgModule({
   declarations: [
