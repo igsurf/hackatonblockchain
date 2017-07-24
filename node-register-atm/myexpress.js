f8b1f758305f8e71e926a81f118e05ccdec479e6
Fernando Taniguti <fernando.taniguti@accenture.com>
2017-07-23 21:16:59 -0300

    Adicionado operações para obter o mac address, e gerar e obter o valor da hash

--- node-register-atm/myexpress.js
+++ node-register-atm/myexpress.js
@@ -1,26 +1,69 @@
 var express=require('express');
 var crypto=require('crypto');
+var fs=require('fs');
+var path=require('path');
+var os=require('os');
 
 var app=express();
 
 app.route('/atm/mac-address').get(function(req, res) {
-	res.send('macaddress');
+	var networkInterface;
+	var networkInterfaceList;
+	var macAddress;
+
+	networkInterfaceList = os.networkInterfaces();
+
+	for (const key of Object.keys(networkInterfaceList)) {
+		networkInterface = networkInterfaceList[key][0];
+		if (networkInterface.address != "127.0.0.1") {
+			macAddress = networkInterface.mac;
+		}
+	}
+
+	if (macAddress) {
+		res.status(200).send(macAddress);
+	} else {
+		res.status(500).send('The MAC address for the ATM was not found.');
+	}
+
 });
 
 app.route('/atm/hash').get(function(req, res) {
-	var id = crypto.randomBytes(20).toString('hex');
-	res.send(id);
+	var hashFilename;
+	var hashValue;
+
+	hashFilename = path.dirname(require.main.filename) + '/hash.txt';
+
+	if(!fs.existsSync(hashFilename)) {
+		res.status(428).send('Hash file not configured in the ATM. Use the POST method to configure the hash file in the ATM');
+	} else {
+		hashValue = fs.readFileSync(hashFilename);
+		res.status(200).send(hashValue);
+	}
 });
 
 app.route('/atm/hash').post(function(req, res) {
-	var id = crypto.randomBytes(20).toString('hex');
-	res.send(id);
-});
+	var hashFilename;
+	var hashValue;
 
-app.get('/', function (req,res) {
-	res.send('Hello World');
+	hashFilename = path.dirname(require.main.filename) + '/hash.txt';
+
+	if(fs.existsSync(hashFilename)) {
+		res.status(403).send('Hash file already configured in ATM. It is forbidden to re-create the hash file once it was created.');
+	} else {
+		hashValue = crypto.randomBytes(20).toString('hex');
+		fs.writeFile(hashFilename, hashValue, function(err) {
+			 if(err) {
+			 	console.log(err);
+			 	res.status(500).send('Error when creating the hash file.');
+			 } else {
+				 res.status(200).send(hashValue);
+			 }
+		});
+	}
+
 });
 
 var server = app.listen(3000, function() {
 
-});
+});	
