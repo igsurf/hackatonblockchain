3b9454d81d81f71ba2f01d9708105fb195f0395c
Igor Abreu Araujo <igor.abreu.araujo@avanade.com>
2017-07-29 16:50:51 -0300

    Alteracoes realizadas

--- blockchain/apps.js
+++ blockchain/apps.js
@@ -13,28 +13,43 @@ app.use(bodyParser());
 
 app.post('/device', (req, res) => {
 
-  device.create((req.body), function(err, doc){
-    console.log(`${err} --- ${doc}` );
-       
-        if(!err){
-            console.log('CADASTRAR');
-        } else {
-            throw err;
-        }
+  device.create((req.body), function (err, doc) {
+
+    if (!err) {
+      console.log('CADASTRAR');
+      res.status("201");
+      return res.end();
+    } else {
+      console.log(`${err} --- ${doc}`);
+      throw err;
+    }
   });
 
-
-//   //let result = deviceContract.registerNewDevice.call('Teste', 'Sao Paulo', 'SP', 'Beazil', {from: web3.eth.accounts[0]});
-//   //console.log(result)
-//   res.end();
+  //   //console.log(result)
+  //   res.end();
 });
 
-// app.get('/get-device', (req, res) => {
-//   const list = deviceContract.getDevices.call({from: web3.eth.accounts[0]})
 
-//   res.json(list);
-//   return res.end();
-// });
+app.get('/get-device', (req, res) => {
+  query = { "cidade": "" };
+  device.find({query}, function (err, doc) {
+
+    if (!err) {
+      console.log('ACHOU');
+     // res.json(doc);
+      console.log(doc);
+      res.status("201");
+
+      return res.end();
+    } else {
+      console.log(`${err} --- ${doc}`);
+      throw err;
+    }
+  });
+
+  // res.json(list);
+  return res.end();
+});
 
 var apps = app.listen(3200, function () {
   // var host = server.address().address
--- blockchain/mongo/env/development.js
+++ blockchain/mongo/env/development.js
@@ -2,9 +2,7 @@
 
 module.exports = {
   db: {
-    uri:'//blockchain:V2bbWKNcS9iRcvNNiyN9Kvx39r49vlNV9Omk51RrBCdxKSuoYxGAgUvm7rEAV2Qq2UCSNxVnuUt8GaRAXEMXWA==@blockchain.documents.azure.com:10255/?ssl=true&sslverifycertificate=false'
+    uri:'localhost:27017/test'
   }
-
-  // mongoClient.connect("mongodb://blockchain:V2bbWKNcS9iRcvNNiyN9Kvx39r49vlNV9Omk51RrBCdxKSuoYxGAgUvm7rEAV2Qq2UCSNxVnuUt8GaRAXEMXWA==@blockchain.documents.azure.com:10255/?ssl=true", function (err, db)
 
 };
